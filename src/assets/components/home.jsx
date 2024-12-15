40c7e578a115741fff84e90bb5ce4703b4a6fe1f
Its-shahbaz-khan <shahbazk.khan100@gmail.com>
2024-12-15 00:00:33 -0800

    complet firt container

--- src/assets/components/home.jsx
+++ src/assets/components/home.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container-fluid Home">
-      <div className="first-container">
+    <div className="container-fluid Home p-0">
+      <div className="first-container ">
+        <div className="container">
         <div className="row">
-          <div className="col-lg-6"></div>
+          <div style={{backgroundImage:URL}} className="col-lg-6">
+            <div>
+              <h6>
+              Best Furniture For Your Castle....
+              </h6>
+              <h1>New Furniture Collection <br/>
+              Trends in 2020</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+              in phasellus non in justo.</p>
+              <button>Shop Now</button>
+            </div>
+          </div>
           <div className="col-lg-6">
        
             <img
@@ -14,7 +26,12 @@ const Home = () => {
             />
           </div>
         </div>
+        </div>
+        <span> &nbsp;</span>
+      <span> </span>
+      <span> </span>
       </div>
+    
     </div>
   );
 };
