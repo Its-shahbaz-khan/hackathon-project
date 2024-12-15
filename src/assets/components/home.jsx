79446f8b6d7d3f245cc6cd32fda02aaa2a7330f2
Its-shahbaz-khan <shahbazk.khan100@gmail.com>
2024-12-14 23:35:33 -0800

    .

--- src/assets/components/home.jsx
+++ src/assets/components/home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const home = () => {
-  return <div className="container">home</div>;
+const Home = () => {
+  return (
+    <div className="container-fluid Home">
+      <div className="first-container">
+        <div className="row">
+          <div className="col-lg-6"></div>
+          <div className="col-lg-6">
+            {/* Reference the image directly */}
+            <img
+              src="/images/Screenshot 2024-12-14 231750.png"
+              alt="Description"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
